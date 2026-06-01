add4c73ccc8e8faf99503452dca14442628190bd
antoniomascarenhas0 <up202504181@up.pt>
2026-06-01 13:07:14 +0100

    Update readSVG.cpp

--- readSVG.cpp
+++ readSVG.cpp
@@ -26,7 +26,52 @@ namespace svg
         {
             string name = child->Value();
 
+            if (name == "ellipse")
+            {
+                Color fill = parse_color(child->Attribute("fill"));
+                Point center = { child->IntAttribute("cx"), child->IntAttribute("cy") };
+                Point radius = { child->IntAttribute("rx"), child->IntAttribute("ry") };
+                svg_elements.push_back(new Ellipse(fill, center, radius));
+            }
+            else if (name == "circle")
+            {
+                Color fill = parse_color(child->Attribute("fill"));
+                Point center = { child->IntAttribute("cx"), child->IntAttribute("cy") };
+                int radius = child->IntAttribute("r");
+                svg_elements.push_back(new Circle(fill, center, radius));
+            }
+            else if (name == "line")
+            {
             
+                const char* stroke_attr = child->Attribute("stroke");
+                Color stroke = stroke_attr ? parse_color(stroke_attr) : Color{0, 0, 0};
+                Point p1 = { child->IntAttribute("x1"), child->IntAttribute("y1") };
+                Point p2 = { child->IntAttribute("x2"), child->IntAttribute("y2") };
+                svg_elements.push_back(new Line(stroke, p1, p2));
+            }
+            else if (name == "rect")
+            {
+                Color fill = parse_color(child->Attribute("fill"));
+                Point topLeft = { child->IntAttribute("x"), child->IntAttribute("y") };
+                int width = child->IntAttribute("width");
+                int height = child->IntAttribute("height");
+                svg_elements.push_back(new Rect(fill, topLeft, width, height));
+            }
+            else if (name == "polygon")
+            {
+                // Polígonos usam estritamente 'fill'
+                const char* fill_attr = child->Attribute("fill");
+                Color fill = fill_attr ? parse_color(fill_attr) : Color{0, 0, 0};
+                std::vector<Point> points = parse_points(child->Attribute("points"));
+                svg_elements.push_back(new Polygon(fill, points));
+            }
+            else if (name == "polyline")
+            {
+                const char* stroke_attr = child->Attribute("stroke");
+                Color stroke = stroke_attr ? parse_color(stroke_attr) : Color{0, 0, 0};
+                std::vector<Point> points = parse_points(child->Attribute("points"));
+                svg_elements.push_back(new Polyline(stroke, points));
+            }
 
 
 
